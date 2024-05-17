39a70658fcbdaef11974812def66458708ee1de9
Chase <chasedbarman@gmail.com>
2024-05-17 15:23:14 -0700

    Makefile

--- backend/server.c
+++ backend/server.c
@@ -6,40 +6,41 @@
 #include <sys/socket.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 
 int main(void) {
-  int server_sock = socket(AF_INET, SOCK_STREAM, 0);
+  int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
   struct sockaddr_in addr;
-  int addr_len = sizeof(addr);
+  socklen_t addr_len = sizeof(addr);
   addr.sin_family = AF_INET;
   addr.sin_addr.s_addr = INADDR_ANY;
   addr.sin_port = htons(8080);
 
-  if (bind(server_sock, (struct sockaddr*) &addr, addr_len) == -1) {
+  if (bind(server_socket, (struct sockaddr*) &addr, addr_len) == -1) {
     perror("Failed to bind socket\n");
     exit(EXIT_FAILURE);
   }
 
-  if (listen(server_sock, 3) == -1) {
+  if (listen(server_socket, 3) == -1) {
     perror("Failed to accept connection\n");
     exit(EXIT_FAILURE);
   }
 
-  int sock;
-  if ((sock = accept(server_sock, (struct sockaddr*) &addr, &addr_len)) == -1) {
+  int client_socket;
+  if ((client_socket = accept(server_socket, (struct sockaddr*) &addr, &addr_len)) == -1) {
     perror("Failed to accept connection\n");
     exit(EXIT_FAILURE);
   }
 
   char buffer[1024];
   strcpy(buffer, "Hello World!\n");
-  if (send(sock, buffer, strlen(buffer), 0) == -1) {
+  if (send(client_socket, buffer, strlen(buffer), 0) == -1) {
     perror("Failed to send data to socket\n");
     exit(EXIT_FAILURE);
   }
 
-  close(sock);
-  close(server_sock);
+  close(client_socket);
+  close(server_socket);
   return 0;
 }
